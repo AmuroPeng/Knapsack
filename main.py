3bd0e11ec43ffc9b11af7d92a70514d3ea35c3d3
AmuroPeng <tiggerpeng1997@gmail.com>
2017-12-08 23:31:35 +0800

    beautify

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import datetime
 import origin
 
 
+# 将重量大于背包容量的物品剔除
 def delete_item(m, w, v):
     list_tuple = [(x, y) for x, y in zip(w, v) if x <= m]
     return [i[0] for i in list_tuple], [i[1] for i in list_tuple]
 
 
+# 按照性价比将物品排序
 def price_ratio(w, v):
     ratio = [float(y / x) for x, y in zip(w, v)]
     zipper = zip(ratio, w, v)
@@ -18,10 +20,6 @@ def price_ratio(w, v):
 
 
 if __name__ == "__main__":
-    # n = 5  # 物品
-    # m = 8  # 容量
-    # w = [1, 2, 3, 4, 5]  # 物品重量
-    # v = [1, 3, 5, 7, 9]  # 物品价格
     n = 10000  # 物品
     m = random.randint(0.5 * n, n)  # 容量
     random_w = range(1, 2 * n)
@@ -32,7 +30,7 @@ if __name__ == "__main__":
     origin.sort(n, m, w, v)
 
     end = datetime.datetime.now()
-    print('未优化时时间:' + str(end - begin))
+    print('优化前时间:' + str(end - begin))
 
     begin = datetime.datetime.now()
     w, v = delete_item(n, w, v)
